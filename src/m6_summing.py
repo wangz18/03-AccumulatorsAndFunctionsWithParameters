d041aebd634c66b583b770ebb95ae9141d87d0cb
wangz18 <wangz18@rose-hulman.edu>
2018-03-11 15:19:44 -0400

    ALL DONE

--- src/m6_summing.py
+++ src/m6_summing.py
@@ -62,7 +62,7 @@ def sum_cosines(n):
 def run_test_sum_square_roots():
     """ Tests the   sum_square_roots   function. """
     # ------------------------------------------------------------------
-    # TODO: 4. Implement this function.
+    # DONE: 4. Implement this function.
     #   It TESTS the  sum_square_roots  function defined below.
     #   Include at least **   3   ** tests.
     #
